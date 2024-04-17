6b1054c644cf2e29f29aafb5f6251e725257d455
Sergio Garguir <sergiogarguir@gmail.com>
2024-04-16 21:23:22 -0300

    2024-04-16 - Arreglos de strings

--- 2024-03-26 - Arreglos primera clase/main.c
+++ 2024-03-26 - Arreglos primera clase/main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <conio.h>
+#include <string.h>
 #include "pila.h"
 
 #define DIM 100
+#define SDIM 3
 #define ESC 27
 
 int cargaArregloRandom(int a[], int dim);
@@ -15,9 +17,25 @@ int esCapicua(int a[], int v);
 void invierteArregloInt(int a[], int v);
 void intercambio(int *a, int *b);
 
+
+
 int main()
 {
     srand(time(NULL));
+    char nombres[DIM][SDIM];
+    int vNombres = 0;
+
+    vNombres = cargaArregloString(SDIM, nombres, vNombres, DIM, "Ingrese un nombre: ");
+    printf("\n <<<<< Listado de Nombres >>>>>\n");
+    muestraArregloString(SDIM, nombres, vNombres);
+
+    int filas=10;
+    int columnas=8;
+    int matriz[filas][columnas];
+
+    cargaMatrizRandom(filas, columnas, matriz);
+    printf("\n Listado de la Matriz \n\n");
+    muestraMatriz(filas, columnas, matriz);
 
     char letras[]={'a','b','c','d'};
     char salludo[]="Hello World";
@@ -153,4 +171,92 @@ void intercambio(int *a, int *b){
     int aux = *a;
     *a = *b;
     *b = aux;
+}
+
+void cargaMatrizRandom(int fil, int col, int m[fil][col]){
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            m[i][j]=rand()%200;
+        }
+    }
+}
+
+void cargaMatrizUsuario(int fil, int col, int m[fil][col]){
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            printf("\n Ingrese una nota: ");
+            scanf("%d", &m[i][j]);
+        }
+    }
+}
+
+void muestraMatriz(int fil, int col, int m[fil][col]){
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            printf(" %4d | ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int sumaMatriz(int fil, int col, int m[fil][col]){
+    int total = 0;
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            total += m[i][j];
+        }
+    }
+    return total;
+}
+
+void muestraArregloString(int d, char a[][d], int v){
+    for(int i=0;i<v;i++){
+        printf("\n %s", a[i]);
+    }
+    printf("\n================================================");
+}
+
+int cargaArregloString(int d, char a[][d], int v, int dim, char label[]){
+    char opcion = 0;
+    char s[d+10];
+    while(v<dim && opcion != 27){
+        printf("\n %s", label);
+        fflush(stdin);
+        gets(s);
+        s[d+10]='\0';
+        if(strlen(s)<d){
+            strcpy(a[v],s);
+            v++;
+        }else{
+            printf("\n Se paso de la dimension");
+            getch();
+        }
+        printf("\n ESC para salir o cualquier tecla para continuar");
+        opcion=getch();
+        system("cls");
+    }
+    return v;
+}
+
+int buscaPosMenorArregloString(int d, char a[][d], int v){
+    int posMenor = 0;
+    for(int i=1;i<v;i++){
+        if(strcmp(a[i],a[posMenor])<0){
+            posMenor = i;
+        }
+    }
+    return posMenor;
+}
+
+int buscoStringEnArreglo(int d, char a[][d], int v, char dato[]){
+    int flag = 0;
+    int i = 0;
+    while(i<v && flag == 0){
+        if(strcmp(a[i],dato)==0){
+            flag = 1;
+        }else{
+            i++;
+        }
+    }
+    return flag;
 }
